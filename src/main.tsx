b4f9ec89e38785d7df54bf264bbc495aec60aaa5
vinayr2125 <vinayr.de@gmail.com>
2025-08-12 21:23:06 +0200

    posthost

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { PostHogProvider } from 'posthog-js/react'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  person_profiles: 'identified_only',
+}
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+      <App />
+    </PostHogProvider>
+  </StrictMode>
+);
